f8b2fa33cc672fae404347fd5633c68620b9cda2
Christian Untiedt <cduntiedt@gmail.com>
2021-10-26 19:26:18 -0500

    -created aws lamba project

---
+++ BBALL.API/LambdaFunction.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BBALL.API
+{
+    public class LambdaFunction : Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder.UseContentRoot(Directory.GetCurrentDirectory()).UseLambdaServer().UseApiGateway();
+        }
+    }
+}
